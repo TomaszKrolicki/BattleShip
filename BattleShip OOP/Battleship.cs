750551f93313ba2c8028f065cc3d1410600ac6b6
Tomasz Królicki <tomek.krolicki@gmail.com>
2022-05-09 14:55:17 +0200

    Comments + BoardFactoryMenu

--- BattleShip OOP/Battleship.cs
+++ BattleShip OOP/Battleship.cs
@@ -10,6 +10,37 @@ namespace BattleShip_OOP
         {
             Display.MainMenu();
             int player = Input.GetNumberFromMenu();
+            // jezeli nowa gra
+            // player 1
+            // wprowadz imie
+            // player 2
+            // wprowadz imie
+            // player 1
+            // wyswietla sie board do ustalenia pozycji statków dla player 1
+            // player 2
+            // wyswietla sie board do ustalenia pozycji statków dla player 2
+
+            // statki do dyspozycji Carrier (1 x 1), Cruiser(1x2), Battleship(1x3), Submarine(1x4), and Destroyer(1x5).
+            // input statkow na board
+            // statki w lini prostej, horyzontalnie lub wertykalnie
+            // statki nie moga laczyc sie ze soba
+            // validacja czy sie da postawic statek, nie wychodzi poza board i czy nie laczy sie z innym 
+            
+
+
+
+            // rozpoczyna sie gra.
+            // player 1 tura - strzelanie 
+            // wyswietlamy zakryty board gracza 2
+            // jezeli trafia to strzela dalej, jesli pudluje - next turn
+            // funkcja sprawdzenia zwyciestwa
+            // player 2 tura - strzelanie 
+            // wyswietlamy zakryty board gracza 1
+            // jezeli trafia to strzela dalej, jesli pudluje - next turn
+            // funkcja sprawdzenia zwyciestwa
+
+            //koniec gry w przypadku zestrzelenia wszystkich statkow przez jednego z playerow
+
         }
     }
 }
--- BattleShip OOP/Input.cs
+++ BattleShip OOP/Input.cs
@@ -8,6 +8,7 @@ namespace BattleShip_OOP
 {
     public class Input
     {
+        
         public static int GetNumberFromMenu()
         {
             string userInput = Console.ReadLine();
@@ -25,6 +26,21 @@ namespace BattleShip_OOP
             return value;
         }
 
-
+        //public static int GetNumberFromMenu()
+        //{
+        //    string userInput = Console.ReadLine();
+        //    int[] valibleInts = { 1, 2, 3 };
+        //    bool isNumber = int.TryParse(userInput, out int value);
+        //    //Console.WriteLine(!isNumber);
+        //    //Console.WriteLine(!valibleInts.Contains(value));
+        //    while (!isNumber || !valibleInts.Contains(value))
+        //    {
+        //        Console.WriteLine("Wrong input!");
+        //        Console.WriteLine("Try again");
+        //        userInput = Console.ReadLine();
+        //        isNumber = int.TryParse(userInput, out value);
+        //    }
+        //    return value;
+        //}
     }
 }
